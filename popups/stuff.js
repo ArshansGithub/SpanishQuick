570970d9e1a9e23da19d7eed00101964209291e3
ArshansGithub <111618520+ArshansGithub@users.noreply.github.com>
2023-04-05 12:28:13 -0700

    Delete stuff.js

--- popups/stuff.js
+++
@@ -1,8 +0,0 @@
-function change() {
-    alert("hi");
-    /*browser.storage.sync.set({
-        type: document.getElementById("ddlViewBy")
-      });*/
-    console.log(document.getElementById("types"));
-    }
-
